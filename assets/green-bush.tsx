725277f2c9c1b578ca4fabdf568dc629141bb2cf
Johannes <59510166+Joonsey@users.noreply.github.com>
2022-08-18 13:53:44 +0200

    fixing assets and making tilemap

---
+++ assets/green-bush.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="green-bush" tilewidth="32" tileheight="32" tilecount="7" columns="7">
+ <image source="tiles/green-bush.png" width="224" height="32"/>
+</tileset>
---
+++ assets/rocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="rocks" tilewidth="32" tileheight="32" tilecount="5" columns="5">
+ <image source="tiles/rocks.png" width="160" height="32"/>
+</tileset>
